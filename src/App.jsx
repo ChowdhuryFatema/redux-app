9f679a1ed18642ba31de32c00c60f63bc443d0b8
ChowdhuryFatema <fatema7chowdhury.ctg@gmail.com>
2024-10-05 06:29:37 -0600

    Completed Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { decNumber, incNumber } from './redux/actions';
 
 const App = () => {
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
+
   return (
-    <div>
-      <h1>Welcome to Redux</h1>
+    <div className='container'>
+      <h1>Increment/Decrement counter</h1>
+      <h4>using React and Redux</h4>
+      <div className='quantity'>
+        <a onClick={() => dispatch(decNumber())} className='quantity__minus' title='Decrement'><span>-</span></a>
+        <input name='quantity' className='quantity__input' type="text" value={myState} />
+        <a onClick={() => dispatch(incNumber())} className='quantity__plus' title='Increment'><span>+</span></a>
+      </div>
     </div>
   );
 };
